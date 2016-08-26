6a348cf48b1b16888317c42b20b740d14fe34b5d
Dave Brown <d.brown@bigdavedev.com>
2016-08-26 13:42:51 +0200

    Remove namespace std
    That's not necessary here
    
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- distance.h
+++ distance.h
@@ -2,235 +2,231 @@
 
 #include <ratio>
 
-namespace std
+namespace units
 {
-    namespace units
+    namespace si
     {
-        namespace si
+        template <typename Rep, typename Length>
+        struct distance;
+
+        namespace detail
         {
-            template <typename Rep, typename Length>
-            struct distance;
-
-            namespace detail
+            template <
+                typename ToDistance,
+                typename Ratio,
+                typename CommonType,
+                bool RatioNumIsOne = false,
+                bool RatioDenIsOne = false>
+            struct distance_cast
             {
-                template <
-                    typename ToDistance,
-                    typename Ratio,
-                    typename CommonType,
-                    bool RatioNumIsOne = false,
-                    bool RatioDenIsOne = false>
-                struct distance_cast
+                template <typename Rep, typename Length>
+                static constexpr ToDistance cast(distance<Rep, Length> from)
                 {
-                    template <typename Rep, typename Length>
-                    static constexpr ToDistance cast(distance<Rep, Length> from)
-                    {
-                        using ToRep = typename ToDistance::rep;
-                        return ToDistance{
-                            static_cast<ToRep>(static_cast<CommonType>(from.count())
-                                               / static_cast<CommonType>(Ratio::num)
-                                               * static_cast<CommonType>(Ratio::den))};
-                    }
-                };
-
-                template <typename ToDistance, typename Ratio, typename CommonType>
-                struct distance_cast<ToDistance, Ratio, CommonType, true, true>
-                {
-                    template <typename Rep, typename Length>
-                    static constexpr ToDistance cast(distance<Rep, Length> from)
-                    {
-                        using ToRep = typename ToDistance::rep;
-                        return ToDistance{static_cast<ToRep>(static_cast<CommonType>(from.count()))};
-                    }
-                };
-
-                template <typename ToDistance, typename Ratio, typename CommonType>
-                struct distance_cast<ToDistance, Ratio, CommonType, true, false>
-                {
-                    template <typename Rep, typename Length>
-                    static constexpr ToDistance cast(distance<Rep, Length> from)
-                    {
-                        using ToRep = typename ToDistance::rep;
-                        return ToDistance{
-                            static_cast<ToRep>(static_cast<CommonType>(from.count())
-                                               * static_cast<CommonType>(Ratio::den))};
-                    }
-                };
-
-                template <typename ToDistance, typename Ratio, typename CommonType>
-                struct distance_cast<ToDistance, Ratio, CommonType, false, true>
-                {
-                    template <typename Rep, typename Length>
-                    static constexpr ToDistance cast(distance<Rep, Length> from)
-                    {
-                        using ToRep = typename ToDistance::rep;
-                        return ToDistance{
-                            static_cast<ToRep>(static_cast<CommonType>(from.count())
-                                               / static_cast<CommonType>(Ratio::num))};
-                    }
-                };
-            }
-
-            template <typename ToDistance, typename Rep, typename Length>
-            constexpr auto distance_cast(distance<Rep, Length> from)
-                -> typename std::enable_if<true, ToDistance>::type
-            {
-                using ToLength   = typename ToDistance::length;
-                using ToRep      = typename ToDistance::rep;
-                using CommonType = typename std::common_type<ToRep, Rep, std::intmax_t>::type;
-                using Ratio      = std::ratio_divide<ToLength, Length>;
-
-                return detail::distance_cast<ToDistance,
-                                             Ratio,
-                                             CommonType,
-                                             Ratio::num == 1,
-                                             Ratio::den == 1>::cast(from);
-            }
-
-            template <typename Rep, typename Length = std::ratio<1>>
-            struct distance
-            {
-                using rep    = Rep;
-                using length = Length;
-
-                constexpr explicit distance(rep value)
-                    : value{value}
-                {
+                    using ToRep = typename ToDistance::rep;
+                    return ToDistance{
+                        static_cast<ToRep>(static_cast<CommonType>(from.count())
+                                           / static_cast<CommonType>(Ratio::num)
+                                           * static_cast<CommonType>(Ratio::den))};
                 }
-
-                template <typename Rep2, typename Length2>
-                constexpr distance(distance<Rep2, Length2> dist)
-                    : value{distance_cast<distance>(dist).count()}
-                {
-                }
-
-                constexpr rep count() const
-                {
-                    return value;
-                }
-
-                distance& operator++()
-                {
-                    ++value;
-                    return *this;
-                }
-
-                distance operator++(int)
-                {
-                    return distance{value + 1};
-                }
-
-                distance& operator--()
-                {
-                    --value;
-                    return *this;
-                }
-
-                distance operator--(int)
-                {
-                    return distance{value - 1};
-                }
-
-                distance& operator+=(distance const other)
-                {
-                    value += other.count();
-                    return *this;
-                }
-
-                distance& operator-=(distance const other)
-                {
-                    value -= other.count();
-                    return *this;
-                }
-
-                distance& operator*=(rep const scalar)
-                {
-                    value *= scalar;
-                    return *this;
-                }
-
-                distance& operator/=(rep const scalar)
-                {
-                    value /= scalar;
-                    return *this;
-                }
-
-                distance& operator%=(rep const scalar)
-                {
-                    value %= scalar;
-                    return *this;
-                }
-
-                distance& operator%=(distance const other)
-                {
-                    value %= other.count();
-                    return *this;
-                }
-
-            private:
-                rep value;
             };
 
-            using nanometres  = distance<long int, std::nano>;
-            using micrometres = distance<long int, std::micro>;
-            using millimetres = distance<long int, std::milli>;
-            using centimetres = distance<long int, std::centi>;
-            using decimetres  = distance<long int, std::deci>;
-            using metres      = distance<long int>;
-            using kilometres  = distance<long int, std::kilo>;
+            template <typename ToDistance, typename Ratio, typename CommonType>
+            struct distance_cast<ToDistance, Ratio, CommonType, true, true>
+            {
+                template <typename Rep, typename Length>
+                static constexpr ToDistance cast(distance<Rep, Length> from)
+                {
+                    using ToRep = typename ToDistance::rep;
+                    return ToDistance{static_cast<ToRep>(static_cast<CommonType>(from.count()))};
+                }
+            };
 
-            // American spellings
-            using nanometers  = nanometres;
-            using micrometers = micrometres;
-            using millimeters = millimetres;
-            using centimeters = centimetres;
-            using decimeters  = decimetres;
-            using meters      = metres;
-            using kilometers  = kilometres;
+            template <typename ToDistance, typename Ratio, typename CommonType>
+            struct distance_cast<ToDistance, Ratio, CommonType, true, false>
+            {
+                template <typename Rep, typename Length>
+                static constexpr ToDistance cast(distance<Rep, Length> from)
+                {
+                    using ToRep = typename ToDistance::rep;
+                    return ToDistance{
+                        static_cast<ToRep>(static_cast<CommonType>(from.count())
+                                           * static_cast<CommonType>(Ratio::den))};
+                }
+            };
+
+            template <typename ToDistance, typename Ratio, typename CommonType>
+            struct distance_cast<ToDistance, Ratio, CommonType, false, true>
+            {
+                template <typename Rep, typename Length>
+                static constexpr ToDistance cast(distance<Rep, Length> from)
+                {
+                    using ToRep = typename ToDistance::rep;
+                    return ToDistance{
+                        static_cast<ToRep>(static_cast<CommonType>(from.count())
+                                           / static_cast<CommonType>(Ratio::num))};
+                }
+            };
         }
 
-        // Non SI units that are interoperable with SI units
-        using yards = si::distance<double, std::ratio<9144, 10000>>;
+        template <typename ToDistance, typename Rep, typename Length>
+        constexpr auto distance_cast(distance<Rep, Length> from)
+            -> typename std::enable_if<true, ToDistance>::type
+        {
+            using ToLength   = typename ToDistance::length;
+            using ToRep      = typename ToDistance::rep;
+            using CommonType = typename std::common_type<ToRep, Rep, intmax_t>::type;
+            using Ratio      = std::ratio_divide<ToLength, Length>;
+
+            return detail::distance_cast<ToDistance,
+                                         Ratio,
+                                         CommonType,
+                                         Ratio::num == 1,
+                                         Ratio::den == 1>::cast(from);
+        }
+
+        template <typename Rep, typename Length = std::ratio<1>>
+        struct distance
+        {
+            using rep    = Rep;
+            using length = Length;
+
+            constexpr explicit distance(rep value)
+                : value{value}
+            {
+            }
+
+            template <typename Rep2, typename Length2>
+            constexpr distance(distance<Rep2, Length2> dist)
+                : value{distance_cast<distance>(dist).count()}
+            {
+            }
+
+            constexpr rep count() const
+            {
+                return value;
+            }
+
+            distance& operator++()
+            {
+                ++value;
+                return *this;
+            }
+
+            distance operator++(int)
+            {
+                return distance{value + 1};
+            }
+
+            distance& operator--()
+            {
+                --value;
+                return *this;
+            }
+
+            distance operator--(int)
+            {
+                return distance{value - 1};
+            }
+
+            distance& operator+=(distance const other)
+            {
+                value += other.count();
+                return *this;
+            }
+
+            distance& operator-=(distance const other)
+            {
+                value -= other.count();
+                return *this;
+            }
+
+            distance& operator*=(rep const scalar)
+            {
+                value *= scalar;
+                return *this;
+            }
+
+            distance& operator/=(rep const scalar)
+            {
+                value /= scalar;
+                return *this;
+            }
+
+            distance& operator%=(rep const scalar)
+            {
+                value %= scalar;
+                return *this;
+            }
+
+            distance& operator%=(distance const other)
+            {
+                value %= other.count();
+                return *this;
+            }
+
+        private:
+            rep value;
+        };
+
+        using nanometres  = distance<long int, std::nano>;
+        using micrometres = distance<long int, std::micro>;
+        using millimetres = distance<long int, std::milli>;
+        using centimetres = distance<long int, std::centi>;
+        using decimetres  = distance<long int, std::deci>;
+        using metres      = distance<long int>;
+        using kilometres  = distance<long int, std::kilo>;
+
+        // American spellings
+        using nanometers  = nanometres;
+        using micrometers = micrometres;
+        using millimeters = millimetres;
+        using centimeters = centimetres;
+        using decimeters  = decimetres;
+        using meters      = metres;
+        using kilometers  = kilometres;
     }
 
-    inline namespace literals
+    // Non SI units that are interoperable with SI units
+    using yards = si::distance<double, std::ratio<9144, 10000>>;
+}
+
+inline namespace literals
+{
+    namespace distance_literals
     {
-        namespace distance_literals
+        constexpr units::si::nanometres operator"" _nm(unsigned long long int dist)
         {
-            constexpr std::units::si::nanometres operator"" _nm(unsigned long long int dist)
-            {
-                return std::units::si::nanometres{static_cast<std::units::si::nanometres::rep>(dist)};
-            }
+            return units::si::nanometres{static_cast<units::si::nanometres::rep>(dist)};
+        }
 
-            constexpr std::units::si::micrometres operator"" _um(unsigned long long int dist)
-            {
-                return std::units::si::micrometres{static_cast<std::units::si::micrometres::rep>(dist)};
-            }
+        constexpr units::si::micrometres operator"" _um(unsigned long long int dist)
+        {
+            return units::si::micrometres{static_cast<units::si::micrometres::rep>(dist)};
+        }
 
-            constexpr std::units::si::millimetres operator"" _mm(unsigned long long int dist)
-            {
-                return std::units::si::millimetres{static_cast<std::units::si::millimetres::rep>(dist)};
-            }
+        constexpr units::si::millimetres operator"" _mm(unsigned long long int dist)
+        {
+            return units::si::millimetres{static_cast<units::si::millimetres::rep>(dist)};
+        }
 
-            constexpr std::units::si::centimetres operator"" _cm(unsigned long long int dist)
-            {
-                return std::units::si::centimetres{static_cast<std::units::si::centimetres::rep>(dist)};
-            }
+        constexpr units::si::centimetres operator"" _cm(unsigned long long int dist)
+        {
+            return units::si::centimetres{static_cast<units::si::centimetres::rep>(dist)};
+        }
 
-            constexpr std::units::si::decimetres operator"" _dm(unsigned long long int dist)
-            {
-                return std::units::si::decimetres{static_cast<std::units::si::decimetres::rep>(dist)};
-            }
+        constexpr units::si::decimetres operator"" _dm(unsigned long long int dist)
+        {
+            return units::si::decimetres{static_cast<units::si::decimetres::rep>(dist)};
+        }
 
-            constexpr std::units::si::metres operator"" _m(unsigned long long int dist)
-            {
-                return std::units::si::metres{static_cast<std::units::si::metres::rep>(dist)};
-            }
+        constexpr units::si::metres operator"" _m(unsigned long long int dist)
+        {
+            return units::si::metres{static_cast<units::si::metres::rep>(dist)};
+        }
 
-            constexpr std::units::si::kilometres operator"" _km(unsigned long long int dist)
-            {
-                return std::units::si::kilometres{static_cast<std::units::si::kilometres::rep>(dist)};
-            }
-
+        constexpr units::si::kilometres operator"" _km(unsigned long long int dist)
+        {
+            return units::si::kilometres{static_cast<units::si::kilometres::rep>(dist)};
         }
     }
 }
--- test/test_distance.cpp
+++ test/test_distance.cpp
@@ -2,14 +2,14 @@
 
 #include "distance.h"
 using namespace testing;
-using namespace std::distance_literals;
+using namespace distance_literals;
 
 namespace TestDistanceUnits
 {
     class DistanceTest : public Test
     {
     protected:
-        std::units::si::distance<long int> distance{1};
+        units::si::distance<long int> distance{1};
     };
 
     TEST_F(DistanceTest, Constructor_WhenInvoked_WillInitialise)
@@ -43,7 +43,7 @@ namespace TestDistanceUnits
 
     TEST_F(DistanceTest, ConstructFromMetresFromKilometres_WillCreateMetresContaining1000)
     {
-        std::units::si::metres metres = 1_km;
+        units::si::metres metres = 1_km;
         EXPECT_EQ(1000, metres.count());
     }
 
@@ -100,7 +100,7 @@ namespace TestDistanceUnits
 
     TEST_F(DistanceTest, Meters_IsIdenticalToMetres)
     {
-        constexpr bool is_same = std::is_same<std::units::si::metres, std::units::si::meters>::value;
+        constexpr bool is_same = std::is_same<units::si::metres, units::si::meters>::value;
         EXPECT_TRUE(is_same);
     }
 
@@ -110,43 +110,43 @@ namespace TestDistanceUnits
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromMetresToMetres_WillYieldMetres)
     {
-        auto metres = std::units::si::distance_cast<std::units::si::metres>(1000_m);
+        auto metres = units::si::distance_cast<units::si::metres>(1000_m);
         EXPECT_EQ(1000, metres.count());
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromMetresToKiloMetres_WillYieldKilometres)
     {
-        auto kilometres = std::units::si::distance_cast<std::units::si::kilometres>(1000_m);
+        auto kilometres = units::si::distance_cast<units::si::kilometres>(1000_m);
         EXPECT_EQ(1, kilometres.count());
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromKilometresToMetres_WillYieldMetres)
     {
-        auto metres = std::units::si::distance_cast<std::units::si::metres>(1_km);
+        auto metres = units::si::distance_cast<units::si::metres>(1_km);
         EXPECT_EQ(1000, metres.count());
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromHalfUnitToWhole_WillRoundDown)
     {
-        auto kilometres = std::units::si::distance_cast<std::units::si::kilometres>(999_m);
+        auto kilometres = units::si::distance_cast<units::si::kilometres>(999_m);
         EXPECT_EQ(0, kilometres.count());
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromMetresToYards_WillYieldYards)
     {
-        auto yards = std::units::si::distance_cast<std::units::yards>(1_m);
+        auto yards = units::si::distance_cast<units::yards>(1_m);
         EXPECT_NEAR(1.09361, yards.count(), 0.00001);
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromKiloMetresToYards_WillYieldYards)
     {
-        auto yards = std::units::si::distance_cast<std::units::yards>(1_km);
+        auto yards = units::si::distance_cast<units::yards>(1_km);
         EXPECT_NEAR(1093.6133, yards.count(), 0.0001);
     }
 
     TEST_F(DistanceCastTest, Cast_WhenCastingFromKilometresToNanometres_WillYieldNanometres)
     {
-        auto nanometres = std::units::si::distance_cast<std::units::si::nanometres>(1_km);
+        auto nanometres = units::si::distance_cast<units::si::nanometres>(1_km);
         EXPECT_EQ(1000000000000, nanometres.count());
     }
 
@@ -156,13 +156,13 @@ namespace TestDistanceUnits
 
     TEST_F(LiteralsTest, MetresLiteral_WhenUsed_WillReturnMetresType)
     {
-        constexpr auto is_same = std::is_same<std::units::si::metres, decltype(1_m)>::value;
+        constexpr auto is_same = std::is_same<units::si::metres, decltype(1_m)>::value;
         EXPECT_TRUE(is_same) << "Incompatible types";
     }
 
     TEST_F(LiteralsTest, KilometresLiteral_WhenUsed_WillReturnKilometresType)
     {
-        constexpr auto is_same = std::is_same<std::units::si::kilometres, decltype(1_km)>::value;
+        constexpr auto is_same = std::is_same<units::si::kilometres, decltype(1_km)>::value;
         EXPECT_TRUE(is_same) << "Incompatible types";
     }
 }
