77fafcf589d952943518733a78879cb37999856e
Dave Brown <d.brown@bigdavedev.com>
2016-08-29 22:02:50 +0200

    Fix formatting
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

--- distance.h
+++ distance.h
@@ -12,28 +12,26 @@ namespace units
 
 namespace std
 {
-    template<typename CommonRep, typename Length1, typename Length2>
+    template <typename CommonRep, typename Length1, typename Length2>
     struct distance_common_type
     {
     private:
-        using gcd_num = utility::greatest_common_divisor<Length1::num, Length2::num>;
-        using gcd_den = utility::greatest_common_divisor<Length1::den, Length2::den>;
+        using gcd_num    = utility::greatest_common_divisor<Length1::num, Length2::num>;
+        using gcd_den    = utility::greatest_common_divisor<Length1::den, Length2::den>;
         using common_rep = typename CommonRep::type;
-        using ratio = std::ratio<gcd_num::value, (Length1::den / gcd_den::value) * Length2::den>;
+        using ratio      = std::ratio<gcd_num::value, (Length1::den / gcd_den::value) * Length2::den>;
 
     public:
         using type = units::distance<common_rep, ratio>;
     };
 
-    template<typename Rep1, typename Length1, typename Rep2, typename Length2>
+    template <typename Rep1, typename Length1, typename Rep2, typename Length2>
     struct common_type<units::distance<Rep1, Length1>,
                        units::distance<Rep2, Length2>>
     {
-        using type = typename distance_common_type<
-                                  std::common_type<Rep1, Rep2>,
-                                  Length1,
-                                  Length2
-                              >::type;
+        using type = typename distance_common_type<std::common_type<Rep1, Rep2>,
+                                                   Length1,
+                                                   Length2>::type;
     };
 }
 
@@ -121,169 +119,116 @@ namespace units
         using length = Length;
 
         constexpr explicit distance(rep value)
-            : value{value}
-        {
-        }
+            : value{value} {}
 
         template <typename Rep2, typename Length2>
         constexpr distance(distance<Rep2, Length2> dist)
-            : value{distance_cast<distance>(dist).count()}
-        {
-        }
+            : value{distance_cast<distance>(dist).count()} {}
 
-        constexpr rep count() const
-        {
-            return value;
-        }
+        constexpr rep count() const { return value; }
 
-        distance& operator++()
-        {
-            ++value;
-            return *this;
-        }
+        distance& operator++()   { ++value; return *this; }
+        distance operator++(int) { return distance{value + 1}; }
+        distance& operator--()   { --value; return *this; }
+        distance operator--(int) { return distance{value - 1}; }
 
-        distance operator++(int)
-        {
-            return distance{value + 1};
-        }
-
-        distance& operator--()
-        {
-            --value;
-            return *this;
-        }
-
-        distance operator--(int)
-        {
-            return distance{value - 1};
-        }
-
-        distance& operator+=(distance const other)
-        {
-            value += other.count();
-            return *this;
-        }
-
-        distance& operator-=(distance const other)
-        {
-            value -= other.count();
-            return *this;
-        }
-
-        distance& operator*=(rep const scalar)
-        {
-            value *= scalar;
-            return *this;
-        }
-
-        distance& operator/=(rep const scalar)
-        {
-            value /= scalar;
-            return *this;
-        }
-
-        distance& operator%=(rep const scalar)
-        {
-            value %= scalar;
-            return *this;
-        }
-
-        distance& operator%=(distance const other)
-        {
-            value %= other.count();
-            return *this;
-        }
+        distance& operator+=(distance const other) { value += other.count(); return *this; }
+        distance& operator-=(distance const other) { value -= other.count(); return *this; }
+        distance& operator*=(rep const scalar)     { value *= scalar;        return *this; }
+        distance& operator/=(rep const scalar)     { value /= scalar;        return *this; }
+        distance& operator%=(rep const scalar)     { value %= scalar;        return *this; }
+        distance& operator%=(distance const other) { value %= other.count(); return *this; }
 
     private:
         rep value;
     };
 
-    template<
+    template <
         typename Rep1,
         typename Length1,
         typename Rep2,
-        typename Length2
-    > constexpr auto operator+(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
+        typename Length2>
+    constexpr auto operator+(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
         -> typename std::common_type<distance<Rep1, Length1>, distance<Rep2, Length2>>::type
     {
-        using distance1 = distance<Rep1, Length1>;
-        using distance2 = distance<Rep2, Length2>;
+        using distance1   = distance<Rep1, Length1>;
+        using distance2   = distance<Rep2, Length2>;
         using common_type = typename std::common_type<distance1, distance2>::type;
 
         return static_cast<common_type>(static_cast<common_type>(lhs).count()
-                                      + static_cast<common_type>(rhs).count());
+                                        + static_cast<common_type>(rhs).count());
     }
 
-    template<
+    template <
         typename Rep1,
         typename Length1,
         typename Rep2,
-        typename Length2
-    > constexpr auto operator-(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
+        typename Length2>
+    constexpr auto operator-(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
         -> typename std::common_type<distance<Rep1, Length1>, distance<Rep2, Length2>>::type
     {
-        using distance1 = distance<Rep1, Length1>;
-        using distance2 = distance<Rep2, Length2>;
+        using distance1   = distance<Rep1, Length1>;
+        using distance2   = distance<Rep2, Length2>;
         using common_type = typename std::common_type<distance1, distance2>::type;
 
         return static_cast<common_type>(static_cast<common_type>(lhs).count()
-                                      - static_cast<common_type>(rhs).count());
+                                        - static_cast<common_type>(rhs).count());
     }
 
-    template<
+    template <
         typename Rep1,
         typename Length,
-        typename Rep2
-    > constexpr auto operator*(distance<Rep1, Length> lhs, Rep2 const scalar)
+        typename Rep2>
+    constexpr auto operator*(distance<Rep1, Length> lhs, Rep2 const scalar)
         -> distance<typename std::common_type<Rep1, Rep2>::type, Length>
     {
         using result_type = distance<typename std::common_type<Rep1, Rep2>::type, Length>;
         return static_cast<result_type>(static_cast<result_type>(lhs).count() * scalar);
     }
 
-    template<
+    template <
         typename Rep1,
         typename Rep2,
-        typename Length
-    > constexpr auto operator*(Rep1 const scalar, distance<Rep2, Length> rhs)
+        typename Length>
+    constexpr auto operator*(Rep1 const scalar, distance<Rep2, Length> rhs)
         -> distance<typename std::common_type<Rep1, Rep2>::type, Length>
     {
         using result_type = distance<typename std::common_type<Rep1, Rep2>::type, Length>;
         return rhs * scalar;
     }
 
-    template<
+    template <
         typename Rep1,
         typename Length,
-        typename Rep2
-    > constexpr auto operator/(distance<Rep1, Length> lhs, Rep2 const scalar)
+        typename Rep2>
+    constexpr auto operator/(distance<Rep1, Length> lhs, Rep2 const scalar)
         -> distance<typename std::common_type<Rep1, Rep2>::type, Length>
     {
         using result_type = distance<typename std::common_type<Rep1, Rep2>::type, Length>;
         return static_cast<result_type>(static_cast<result_type>(lhs).count() / scalar);
     }
 
-    template<
+    template <
         typename Rep1,
         typename Length1,
         typename Rep2,
-        typename Length2
-    > constexpr auto operator%(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
+        typename Length2>
+    constexpr auto operator%(distance<Rep1, Length1> lhs, distance<Rep2, Length2> rhs)
         -> typename std::common_type<distance<Rep1, Length1>, distance<Rep2, Length2>>::type
     {
-        using distance1 = distance<Rep1, Length1>;
-        using distance2 = distance<Rep2, Length2>;
+        using distance1   = distance<Rep1, Length1>;
+        using distance2   = distance<Rep2, Length2>;
         using common_type = typename std::common_type<distance1, distance2>::type;
 
         return static_cast<common_type>(static_cast<common_type>(lhs).count()
-                                      % static_cast<common_type>(rhs).count());
+                                        % static_cast<common_type>(rhs).count());
     }
 
-    template<
+    template <
         typename Rep1,
         typename Length,
-        typename Rep2
-    > constexpr auto operator%(distance<Rep1, Length> lhs, Rep2 const scalar)
+        typename Rep2>
+    constexpr auto operator%(distance<Rep1, Length> lhs, Rep2 const scalar)
         -> distance<typename std::common_type<Rep1, Rep2>::type, Length>
     {
         using result_type = distance<typename std::common_type<Rep1, Rep2>::type, Length>;
