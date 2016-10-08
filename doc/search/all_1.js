578b70432a398d9b0e39e731a62a2c4551326714
Dave Brown <d.brown@bigdavedev.com>
2016-10-08 11:38:18 +0200

    Add doxygen make target
    Signed-off-by: Dave Brown <d.brown@bigdavedev.com>

---
+++ doc/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['distance',['distance',['../structunits_1_1distance.html',1,'units']]],
+  ['distance_5fcommon_5ftype',['distance_common_type',['../structstd_1_1distance__common__type.html',1,'std']]]
+];
---
+++ doc/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['common_5ftype_3c_20units_3a_3adistance_3c_20rep1_2c_20length1_20_3e_2c_20units_3a_3adistance_3c_20rep2_2c_20length2_20_3e_20_3e',['common_type&lt; units::distance&lt; Rep1, Length1 &gt;, units::distance&lt; Rep2, Length2 &gt; &gt;',['../structstd_1_1common__type_3_01units_1_1distance_3_01Rep1_00_01Length1_01_4_00_01units_1_1distanc5ed2eb36acd45d3c013f7cf491b10afb.html',1,'std']]]
+];
