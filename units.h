6a3b6319e58f6796625610a35979fc0963a6dbcc
David Brown <d.brown@bigdavedev.com>
2017-04-02 20:14:33 +0200

    Update copyright notice
    Signed-off-by: David Brown <d.brown@bigdavedev.com>

--- units.h
+++ units.h
@@ -3,7 +3,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2016 David Brown
+ * Copyright (c) 2016-2017 David Brown
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
