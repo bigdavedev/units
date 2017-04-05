4325ea473e2ceb9ff04f0ad2540014d9e620455b
David Brown <d.brown@bigdavedev.com>
2017-04-05 10:23:44 +0200

    Reorganised unit test structure
    Signed-off-by: David Brown <d.brown@bigdavedev.com>
    
    #15

--- test/test_distance.cpp
+++ test/test_distance.cpp
@@ -6,169 +6,183 @@ using namespace distance_literals;
 
 namespace TestDistanceUnits
 {
-	class DistanceTest : public Test
+	class UnitConstructorTest : public Test
 	{
 	protected:
 		units::distance<double> distance{1};
 	};
 
-	TEST_F(DistanceTest, Constructor_WhenInvoked_WillInitialise)
+	TEST_F(UnitConstructorTest, Constructor_WhenInvoked_WillInitialise)
 	{
-		EXPECT_EQ(1, distance.count());
+		units::metres metres{1};
+		EXPECT_EQ(1, metres.count());
 	}
 
-	TEST_F(DistanceTest, PreIncrementOperator_WillIncrementDistanceByOne)
+	TEST_F(UnitConstructorTest, Constructor_WhenInokedWithDifferentCompatibleUnit_WillConvertToCurrentUnit)
 	{
-		++distance;
+		units::metres metres{1_km};
+		EXPECT_EQ(1000, metres.count());
+	}
+
+	class UnitIncrementDecrementTest : public UnitConstructorTest
+	{
+	};
+
+	TEST_F(UnitIncrementDecrementTest, PreIncrementOperator_WillIncrementDistanceByOne)
+	{
+		auto result = ++distance;
 		EXPECT_EQ(2, distance.count());
+		EXPECT_EQ(2, result.count());
 	}
 
-	TEST_F(DistanceTest, PostIncrementOperator_WillIncrementDistanceByOne)
+	TEST_F(UnitIncrementDecrementTest, PostIncrementOperator_WillIncrementDistanceByOne)
 	{
 		auto result = distance++;
 		EXPECT_EQ(2, distance.count());
 		EXPECT_EQ(1, result.count());
 	}
 
-	TEST_F(DistanceTest, PreDecrementOperator_WillDecrementDistanceByOne)
+	TEST_F(UnitIncrementDecrementTest, PreDecrementOperator_WillDecrementDistanceByOne)
 	{
-		--distance;
+		auto result = --distance;
 		EXPECT_EQ(0, distance.count());
+		EXPECT_EQ(0, result.count());
 	}
 
-	TEST_F(DistanceTest, PostDecrementOperator_WillDecrementDistanceByOne)
+	TEST_F(UnitIncrementDecrementTest, PostDecrementOperator_WillDecrementDistanceByOne)
 	{
 		auto result = distance--;
 		EXPECT_EQ(0, distance.count());
 		EXPECT_EQ(1, result.count());
 	}
 
-	TEST_F(DistanceTest, ConstructFromMetresFromKilometres_WillCreateMetresContaining1000)
+	class UnitCompoundAssignmentTest : public UnitConstructorTest
 	{
-		units::metres metres = 1_km;
-		EXPECT_NEAR(1000.0, metres.count(), 0.000000001);
-	}
+	};
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenAddingMetresToMetres_WillResultInMetresAddedToCurrentMetres)
+	TEST_F(UnitCompoundAssignmentTest, CompoundAdditionAssignment_WhenAddingMetresToMetres_WillResultInMetresAddedToCurrentMetres)
 	{
 		distance += 1_m;
 		EXPECT_EQ(2, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenAddingKilometresToMetres_WillResultInKilometresAddedToCurrentMetres)
+	TEST_F(UnitCompoundAssignmentTest, CompoundAdditionAssignment_WhenAddingKilometresToMetres_WillResultInKilometresAddedToCurrentMetres)
 	{
 		distance += 1_km;
 		EXPECT_EQ(1001, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenSubtractingMetresToMetres_WillResultInMetresSubtractedToCurrentMetres)
+	TEST_F(UnitCompoundAssignmentTest, CompoundSubtractionAssignment_WhenSubtractingMetresToMetres_WillResultInMetresSubtractedToCurrentMetres)
 	{
 		distance -= 1_m;
 		EXPECT_EQ(0, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenSubtractingKilometresToMetres_WillResultInKilometresSubtractedToCurrentMetres)
+	TEST_F(UnitCompoundAssignmentTest, CompoundSubtractionAssignment_WhenSubtractingKilometresToMetres_WillResultInKilometresSubtractedToCurrentMetres)
 	{
 		distance -= 1_km;
 		EXPECT_EQ(-999, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenMultiplyingByScalar_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundMultiplicationAssignment_WhenMultiplyingByScalar_WillUpdateValue)
 	{
 		distance *= 10;
 		EXPECT_EQ(10, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenDividingByScalar_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundDivisionAssignment_WhenDividingByScalar_WillUpdateValue)
 	{
 		distance = 10_m;
 		distance /= 2;
 		EXPECT_EQ(5, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenGettingRemainderByScalar_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundRemainderAssignment_WhenGettingRemainderByScalar_WillUpdateValue)
 	{
 		distance = 2531_m;
 		distance %= 1000;
 		EXPECT_EQ(531, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenGettingRemainderByDistance_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundRemainderAssignment_WhenGettingRemainderByDistance_WillUpdateValue)
 	{
 		distance = 2531_m;
 		distance %= 1_km;
 		EXPECT_EQ(531, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenGettingRemainderByIntScalar_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundRemainderAssignment_WhenGettingRemainderByIntScalar_WillUpdateValue)
 	{
 		distance = units::distance<int>{2531};
 		distance %= 1000;
 		EXPECT_EQ(531, distance.count());
 	}
 
-	TEST_F(DistanceTest, CompoundAssignment_WhenGettingRemainderByIntDistance_WillUpdateValue)
+	TEST_F(UnitCompoundAssignmentTest, CompoundRemainderAssignment_WhenGettingRemainderByIntDistance_WillUpdateValue)
 	{
 		distance = units::distance<int>{2531};
 		distance %= 1_km;
 		EXPECT_EQ(531, distance.count());
 	}
 
-	TEST_F(DistanceTest, Meters_IsIdenticalToMetres)
+	class UnitArithmeticOperationsTest : public Test
 	{
-		constexpr bool is_same = std::is_same<units::metres, units::meters>::value;
-		EXPECT_TRUE(is_same);
-	}
+	};
 
-	TEST_F(DistanceTest, OperatorPlus_WhenUsed_WillAddMetresToMetres)
+	TEST_F(UnitArithmeticOperationsTest, OperatorPlus_WhenUsed_WillAddMetresToMetres)
 	{
-		auto result = distance + 1_m;
+		auto result = 1_m + 1_m;
 		EXPECT_EQ(2, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorPlus_WhenUsed_WillAddKilometresToMetres)
+	TEST_F(UnitArithmeticOperationsTest, OperatorPlus_WhenUsed_WillAddKilometresToMetres)
 	{
-		auto result = distance + 1_km;
+		auto result = 1_m + 1_km;
 		EXPECT_EQ(1001, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorMinus_WhenUsed_WillSubtractMetresFromMetres)
+	TEST_F(UnitArithmeticOperationsTest, CreatingMetresFromKilometres_WillYieldMetres)
 	{
-		auto result = distance - 1_m;
+		EXPECT_EQ(1000, units::metres{1_km}.count());
+	}
+
+	TEST_F(UnitArithmeticOperationsTest, OperatorMinus_WhenUsed_WillSubtractMetresFromMetres)
+	{
+		auto result = 1_m - 1_m;
 		EXPECT_EQ(0, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorMinus_WhenUsed_WillSubtractMetresFromKilometres)
+	TEST_F(UnitArithmeticOperationsTest, OperatorMinus_WhenUsed_WillSubtractMetresFromKilometres)
 	{
-		auto result = 1_km - distance;
+		auto result = 1_km - 1_m;
 		EXPECT_EQ(999, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorMultiply_WhenUsed_WillMultiplyMetresToScalar)
+	TEST_F(UnitArithmeticOperationsTest, OperatorMultiply_WhenUsed_WillMultiplyMetresToScalar)
 	{
-		auto result = distance * 2;
+		auto result = 1_m * 2;
 		EXPECT_EQ(2, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorMultiply_WhenUsed_WillMultiplyScalarToMetres)
+	TEST_F(UnitArithmeticOperationsTest, OperatorMultiply_WhenUsed_WillMultiplyScalarToMetres)
 	{
-		auto result = 2 * distance;
+		auto result = 2 * 1_m;
 		EXPECT_EQ(2, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorDivide_WhenUsed_WillDivideMetresByScalar)
+	TEST_F(UnitArithmeticOperationsTest, OperatorDivide_WhenUsed_WillDivideMetresByScalar)
 	{
 		auto result = 2_m / 2;
 		EXPECT_EQ(1, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorRemainderInt_WhenGettingRemainderByDistance_WillGiveRemainder)
+	TEST_F(UnitArithmeticOperationsTest, OperatorRemainderInt_WhenGettingRemainderByDistance_WillGiveRemainder)
 	{
-		auto const result = units::distance<int>{ 2531 } % units::distance<int, std::kilo>{1};
+		auto const result = units::distance<int>{2531} % units::distance<int, std::kilo>{1};
 		EXPECT_EQ(531, result.count());
 	}
 
-	TEST_F(DistanceTest, OperatorRemainderInt_WhenGettingRemainderByScalar_WillGiveRemainder)
+	TEST_F(UnitArithmeticOperationsTest, OperatorRemainderInt_WhenGettingRemainderByScalar_WillGiveRemainder)
 	{
 		auto const result = units::distance<int>{2531} % 1000;
 		EXPECT_EQ(531, result.count());
@@ -237,6 +251,32 @@ namespace TestDistanceUnits
 		auto nanometres = units::distance_cast<units::nanometres>(1_km);
 		EXPECT_NEAR(1000000000000, nanometres.count(), 0.0000001);
 	}
+
+	template <typename T>
+	class BritishVsAmericanSpellingTest : public Test
+	{
+	};
+
+	TYPED_TEST_CASE_P(BritishVsAmericanSpellingTest);
+
+	TYPED_TEST_P(BritishVsAmericanSpellingTest, MetresVsMetersYieldsSameType)
+	{
+		constexpr bool is_same = std::is_same<typename std::tuple_element<0, TypeParam>::type,
+		                                      typename std::tuple_element<1, TypeParam>::type>::value;
+		EXPECT_TRUE(is_same);
+	}
+
+	using BritishVsAmericanSpellingTuple = Types<std::tuple<units::nanometres, units::nanometers>,
+	                                             std::tuple<units::micrometres, units::micrometers>,
+	                                             std::tuple<units::millimetres, units::millimeters>,
+	                                             std::tuple<units::centimetres, units::centimeters>,
+	                                             std::tuple<units::decimetres, units::decimeters>,
+	                                             std::tuple<units::metres, units::meters>,
+	                                             std::tuple<units::kilometres, units::kilometers>>;
+
+	REGISTER_TYPED_TEST_CASE_P(BritishVsAmericanSpellingTest, MetresVsMetersYieldsSameType);
+
+	INSTANTIATE_TYPED_TEST_CASE_P(BritishVsAmericanSpelling, BritishVsAmericanSpellingTest, BritishVsAmericanSpellingTuple);
 
 	template <typename T>
 	class LiteralsTest : public Test
