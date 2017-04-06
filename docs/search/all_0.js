dd52a9fc7b429c25e99dc908b7471fef14c44adf
David Brown <d.brown@bigdavedev.com>
2017-04-06 10:46:26 +0200

    Update documentation
    Signed-off-by: David Brown <d.brown@bigdavedev.com>

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['common_5ftype_3c_20units_3a_3adistance_3c_20rep1_2c_20length1_20_3e_2c_20units_3a_3adistance_3c_20rep2_2c_20length2_20_3e_20_3e',['common_type&lt; units::distance&lt; Rep1, Length1 &gt;, units::distance&lt; Rep2, Length2 &gt; &gt;',['../structstd_1_1common__type_3_01units_1_1distance_3_01Rep1_00_01Length1_01_4_00_01units_1_1distanc5ed2eb36acd45d3c013f7cf491b10afb.html',1,'std']]]
+  ['common_5ftype_3c_20units_3a_3adistance_3c_20rep1_2c_20length1_20_3e_2c_20units_3a_3adistance_3c_20rep2_2c_20length2_20_3e_20_3e',['common_type&lt; units::distance&lt; Rep1, Length1 &gt;, units::distance&lt; Rep2, Length2 &gt; &gt;',['../structstd_1_1common__type_3_01units_1_1distance_3_01_rep1_00_01_length1_01_4_00_01units_1_1dista2c9c38d9d07aa2e25a816f28a0dc2f6a.html',1,'std']]]
 ];
--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['is_5fdistance',['is_distance',['../structunits_1_1is__distance.html',1,'units']]],
-  ['is_5fdistance_3c_20distance_3c_20rep_2c_20length_20_3e_20_3e',['is_distance&lt; distance&lt; Rep, Length &gt; &gt;',['../structunits_1_1is__distance_3_01distance_3_01Rep_00_01Length_01_4_01_4.html',1,'units']]]
+  ['is_5fdistance_3c_20distance_3c_20rep_2c_20length_20_3e_20_3e',['is_distance&lt; distance&lt; Rep, Length &gt; &gt;',['../structunits_1_1is__distance_3_01distance_3_01_rep_00_01_length_01_4_01_4.html',1,'units']]]
 ];
