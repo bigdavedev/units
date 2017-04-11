d85772cfb671d6168d6094e1da88f21a46a7f0de
David Brown <d.brown@bigdavedev.com>
2017-04-11 13:59:30 +0200

    Move stream buffer in to test case instead
    Signed-off-by: David Brown <d.brown@bigdavedev.com>

--- test/test_streams.cpp
+++ test/test_streams.cpp
@@ -17,50 +17,48 @@ namespace TestStreamOutput
 {
 	namespace
 	{
-		auto literal_to_string_lookup = std::unordered_map<std::type_index, std::string>
-		    {{std::type_index{typeid(units::nanometres)}, std::string{"nm"}},
-		     {std::type_index{typeid(units::micrometres)}, std::string{"um"}},
-		     {std::type_index{typeid(units::millimetres)}, std::string{"mm"}},
-		     {std::type_index{typeid(units::centimetres)}, std::string{"cm"}},
-		     {std::type_index{typeid(units::decimeters)}, std::string{"dm"}},
-		     {std::type_index{typeid(units::metres)}, std::string{"m"}},
-		     {std::type_index{typeid(units::kilometres)}, std::string{"km"}},
-		     {std::type_index{typeid(units::thous)}, std::string{"th"}},
-		     {std::type_index{typeid(units::inches)}, std::string{"in"}},
-		     {std::type_index{typeid(units::links)}, std::string{"li"}},
-		     {std::type_index{typeid(units::feet)}, std::string{"ft"}},
-		     {std::type_index{typeid(units::yards)}, std::string{"yd"}},
-		     {std::type_index{typeid(units::rods)}, std::string{"rd"}},
-		     {std::type_index{typeid(units::chains)}, std::string{"ch"}},
-		     {std::type_index{typeid(units::furlongs)}, std::string{"fur"}},
-		     {std::type_index{typeid(units::miles)}, std::string{"mi"}},
-		     {std::type_index{typeid(units::leagues)}, std::string{"lea"}},
-		     {std::type_index{typeid(units::fathoms)}, std::string{"ftm"}},
-		     {std::type_index{typeid(units::cables)}, std::string{"cb"}},
-		     {std::type_index{typeid(units::nautical_miles)}, std::string{"nmi"}},
-		     {std::type_index{typeid(units::earth_radii)}, std::string{"R"}},
-		     {std::type_index{typeid(units::lunar_distances)}, std::string{"LD"}},
-		     {std::type_index{typeid(units::astronimical_units)}, std::string{"AU"}},
-		     {std::type_index{typeid(units::light_years)}, std::string{"ly"}},
-		     {std::type_index{typeid(units::parsecs)}, std::string{"pc"}}};
+		std::unordered_map<std::type_index, std::string> literal_to_string_lookup =
+		    {{std::type_index{typeid(units::nanometres)}, "nm"},
+		     {std::type_index{typeid(units::micrometres)}, "um"},
+		     {std::type_index{typeid(units::millimetres)}, "mm"},
+		     {std::type_index{typeid(units::centimetres)}, "cm"},
+		     {std::type_index{typeid(units::decimeters)}, "dm"},
+		     {std::type_index{typeid(units::metres)}, "m"},
+		     {std::type_index{typeid(units::kilometres)}, "km"},
+		     {std::type_index{typeid(units::thous)}, "th"},
+		     {std::type_index{typeid(units::inches)}, "in"},
+		     {std::type_index{typeid(units::links)}, "li"},
+		     {std::type_index{typeid(units::feet)}, "ft"},
+		     {std::type_index{typeid(units::yards)}, "yd"},
+		     {std::type_index{typeid(units::rods)}, "rd"},
+		     {std::type_index{typeid(units::chains)}, "ch"},
+		     {std::type_index{typeid(units::furlongs)}, "fur"},
+		     {std::type_index{typeid(units::miles)}, "mi"},
+		     {std::type_index{typeid(units::leagues)}, "lea"},
+		     {std::type_index{typeid(units::fathoms)}, "ftm"},
+		     {std::type_index{typeid(units::cables)}, "cb"},
+		     {std::type_index{typeid(units::nautical_miles)}, "nmi"},
+		     {std::type_index{typeid(units::earth_radii)}, "R"},
+		     {std::type_index{typeid(units::lunar_distances)}, "LD"},
+		     {std::type_index{typeid(units::astronimical_units)}, "AU"},
+		     {std::type_index{typeid(units::light_years)}, "ly"},
+		     {std::type_index{typeid(units::parsecs)}, "pc"}};
 	}
 
 	template <typename T>
 	class StreamOutputTest : public Test
 	{
-	protected:
-		std::stringstream buffer;
 	};
 
 	TYPED_TEST_CASE_P(StreamOutputTest);
 
 	TYPED_TEST_P(StreamOutputTest, StreamInsertionOperator_WillFormatStringCorrectly)
 	{
-		buffer << TypeParam{1};
+		std::stringstream buffer{ "" };
+		buffer << TypeParam{ 1 };
 
-		std::stringstream buffer_expected{""};
-		buffer_expected << "1" << literal_to_string_lookup[std::type_index{typeid(TypeParam)}];
-		EXPECT_EQ(buffer_expected.str(), buffer.str());
+		std::string expected {"1" + literal_to_string_lookup[std::type_index{typeid(TypeParam)}]};
+		EXPECT_EQ(expected, buffer.str());
 	}
 
 	using DistanceTuple = Types<units::nanometres,
